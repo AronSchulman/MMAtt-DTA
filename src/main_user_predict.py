51bb620493a7bf8f32ab9e778ab533246660cb97
AronSchulman <63584295+AronSchulman@users.noreply.github.com>
2024-07-12 12:53:52 +0300

    Improve featurization efficiency

--- src/main_user_predict.py
+++ src/main_user_predict.py
@@ -14,8 +14,6 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_path", action="store", help="Provide user data input file path.", default="../example_input.csv")
-    parser.add_argument("-j", "--jsonfile_path", action="store", help="Provide model config params.json file path.", default="../json_files/model_config_params.json")
-    parser.add_argument("-m", "--model_path", action="store", help="Provide model directory path to the directory containing 'pchembl_models' and 'interaction_score_models' directories.", default="../")
     args = parser.parse_args()
     df_user_input = pd.read_csv(args.input_path).dropna()
     
@@ -23,21 +21,21 @@ def main():
     cols.append("prediction")
     df_final = pd.DataFrame(columns=cols).dropna(axis=1, how='all') # dropna to avert warning of concatenation to empty dataframes
     
-    with open(args.jsonfile_path, 'r') as json_file: # config for model architecture & hyperparams
+    with open("../json_files/model_config_params.json", 'r') as json_file: # config for model architecture & hyperparams
         cfg_all = json.load(json_file)
+
+    zernikes = pd.read_csv("../descriptor_data/zernike_descriptors.csv")
+    seq_based = pd.read_csv("../descriptor_data/protein_sequence_based_features.csv")
     
     for prot in df_user_input.protein_class.unique(): # process by protein class
         df_user_per_prot = df_user_input[df_user_input.protein_class==prot]
         cfg = cfg_all[prot]
         for mod_type in df_user_per_prot.model_type.unique():# process by model type
             df_per_prot_and_mod = df_user_per_prot[df_user_per_prot.model_type==mod_type].copy()
-            for i in range(len(df_per_prot_and_mod)): # process one line at a time - could be made more efficient!
-                input_line = df_per_prot_and_mod.iloc[i]
-                if i == 0:
-                    selected_features = prepare_model_input(input_line.smiles, input_line.uniprot_id, input_line.protein_class)
-                else:
-                    selected_features = pd.concat([selected_features, prepare_model_input(input_line.smiles, input_line.uniprot_id, input_line.protein_class)])
-    
+            df_featurize = df_per_prot_and_mod[["uniprot_id", "smiles"]]
+            subclasses = pd.read_csv(f"../descriptor_data/subclass_data/{prot}_subclass_labels.csv")
+            feature_selection = pd.read_csv(f"../descriptor_data/feature_selections/{prot}_selected_features.csv")
+            selected_features = prepare_model_input(df_featurize, zernikes, seq_based, subclasses, feature_selection)
     
             dataloader, shapes = prepare_dataloader(selected_features, pd.Series(selected_features.index), cfg)
     
@@ -45,7 +43,7 @@ def main():
             model_prot = HalfBlock(shapes[2], shapes[3], cfg, "protein").to(device)
             model_combined = AttentionDTINN(cfg, model_comp, model_prot).to(device)
     
-            weights_dir = f"{args.model_path}/{input_line.model_type}_models/{input_line.protein_class}"
+            weights_dir = f"../models/{mod_type}_models/{prot}"
             ensemble_preds = []
             for i in range(5):
                 with open(f"{weights_dir}/model_{i}/dict_checkpoint.pkl", "rb") as f:
@@ -56,6 +54,7 @@ def main():
             mean_preds = np.mean(ensemble_preds, axis=0)
             df_per_prot_and_mod["prediction"] = mean_preds
             df_final = pd.concat([df_final, df_per_prot_and_mod])
+        print("Done", prot)
     
     df_final = df_final.sort_index()
     df_final.to_csv("model_output_predictions.csv", index=False)
